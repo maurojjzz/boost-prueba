4d6a6dbc608b902c4ab9419d65d553b510234e6e
maurojjzz <maurojim123@gmail.com>
2024-08-31 15:44:38 -0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,105 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Typography, useTheme, Link } from "@mui/material";
 
 const Header = () => {
   const theme = useTheme();
   return (
     <Box
       sx={{
-        border: "2px solid white",
         textAlign: "center",
+        pt: "50px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <Typography
-        variant="h4"
+      <Box
         sx={{
-          color: theme.palette.primary.main,
-          fontFamily: "Unison-Pro-Bold",
-          textAlign: "center",
-          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-          fontSize: {
-            xs: "calc(1.3rem + 4vw)",
-            sm: "calc(2rem + 4vw)",
-          },
-          border: "1px solid red",
-          lineHeight: "100%",
-          display: "inline-block",
-          whiteSpace: "nowrap",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        Boost Car
-      </Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            color: theme.palette.primary.main,
+            fontFamily: "Unison-Pro-Bold",
+            textAlign: "center",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.8)",
+            fontSize: {
+              xs: "calc(1.3rem + 4vw)",
+              sm: "calc(2rem + 4vw)",
+            },
+            lineHeight: "100%",
+            display: "inline-block",
+            whiteSpace: "nowrap",
+          }}
+        >
+          Boost Car
+        </Typography>
+        <Box
+          sx={{
+            color: theme.palette.primary.main,
+            fontFamily: "Roboto",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 1)",
+            mt: "10px",
+          }}
+        >
+          <ul
+            style={{
+              listStyle: "none",
+              display: "flex",
+              justifyContent: "flex-end",
+              gap: "5px",
+              padding: 0,
+              margin: 0,
+              fontWeight: "400",
+            }}
+          >
+            <li>
+              <Link
+                href="#home"
+                sx={{
+                  color: "inherit",
+                  textDecoration: "none",
+                  "&:hover": {
+                    textDecoration: "underline",
+                  },
+                }}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#services"
+                sx={{
+                  color: "inherit",
+                  textDecoration: "none",
+                  "&:hover": {
+                    textDecoration: "underline",
+                  },
+                }}
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#contact"
+                sx={{
+                  color: "inherit",
+                  textDecoration: "none",
+                  "&:hover": {
+                    textDecoration: "underline",
+                  },
+                }}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </Box>
+      </Box>
     </Box>
   );
 };
