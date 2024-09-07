9284d990d3f221153def0bdea7b4fcd6f2229881
maurojjzz <maurojim123@gmail.com>
2024-09-07 18:06:17 -0300

    hotfix on filters

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
 import { ServiceHTTP } from "../../utils/services/ServiceHTTP.js";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Select,
+  FormControl,
+  MenuItem,
+  Checkbox,
+  FormControlLabel,
+  InputLabel,
+  useTheme,
+} from "@mui/material";
 import CarProduct from "./cardProduct/CardProduct.jsx";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
   const [marcas, setMarcas] = useState([]);
 
+  const [selectedBrand, setSelectedBrand] = useState("");
+  const [showUsed, setShowUsed] = useState(false);
+  const [sortOrder, setSortOrder] = useState("desc");
+
+  const theme = useTheme();
 
   const brands = new ServiceHTTP("brand");
   const cars = new ServiceHTTP("car");
@@ -37,29 +52,48 @@ const Product = () => {
     getBrands();
   }, []);
 
+  const handleBrandChange = (event) => {
+    setSelectedBrand(event.target.value);
+  };
+
+  const handleUsageChange = (event) => {
+    setShowUsed(event.target.checked);
+  };
+
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const filteredAndSortedProducts = products
+    .filter(
+      (product) =>
+        (selectedBrand ? product.brandId === selectedBrand : true) && (showUsed ? product.usage === true : true)
+    )
+    .sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.price - b.price;
+      } else {
+        return b.price - a.price;
+      }
+    });
 
   return (
     <Box
       sx={{
         display: "flex",
-        flexGrow: 1, 
+        flexGrow: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "20px",
-          justifyContent: "space-around",
-        }}
-      >
-        <Typography
+      <Typography
         variant="h3"
         sx={{
           alignSelf: "flex-start",
           fontFamily: "HelveticaNowDisplay",
-          maxWidth: "1200px" ,
+          maxWidth: "900px",
+          width: { sm: "75%" },
           textAlign: "start",
           ml: "15px",
           my: "50px",
@@ -71,13 +105,112 @@ const Product = () => {
       >
         Nuestro catalogo de vehiculos
       </Typography>
-        {products.map((product) => (
+
+      <Box
+        sx={{
+          mb: 2,
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          maxWidth: "900px",
+          justifyContent: "space-between",
+          width: "90%",
+          p: "15px",
+          borderRadius: "5px",
+          gap: "10px",
+        }}
+      >
+        <FormControl
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+          }}
+        >
+          <InputLabel
+            sx={{
+              color: theme.palette.black.main,
+              fontFamily: "HelveticaNowDisplay",
+              fontSize: "18px",
+              letterSpacing: "1px",
+              "&.Mui-focused": {
+                color: theme.palette.black.main,
+                backgroundColor: theme.palette.primary.main,
+              },
+            }}
+          >
+            Marca
+          </InputLabel>
+          <Select
+            value={selectedBrand}
+            onChange={handleBrandChange}
+            label="Marca"
+            sx={{
+              "&.Mui-focused": {
+                border: "1px solid #c3c4c3",
+              },
+              outline: "none",
+            }}
+          >
+            <MenuItem value="">
+              <em>Todas las marcas</em>
+            </MenuItem>
+            {marcas.map((marca) => (
+              <MenuItem key={marca.id} value={marca.id}>
+                {marca.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControlLabel
+          sx={{
+            "& .MuiCheckbox-root.Mui-checked": {
+              color: "green",
+            },
+          }}
+          control={<Checkbox checked={showUsed} onChange={handleUsageChange} />}
+          label="Mostrar solo autos usados"
+        />
+
+        <FormControl
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+          }}
+        >
+          <InputLabel
+            sx={{
+              color: theme.palette.black.main,
+              fontFamily: "HelveticaNowDisplay",
+              fontSize: "18px",
+              "&.Mui-focused": {
+                color: theme.palette.black.main,
+                backgroundColor: theme.palette.primary.main,
+              },
+            }}
+          >
+            Ordenar por Precio
+          </InputLabel>
+          <Select value={sortOrder} onChange={handleSortChange} label="Ordenar por Precio">
+            <MenuItem value="asc">De menor a mayor</MenuItem>
+            <MenuItem value="desc">De mayor a menor</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          justifyContent: "space-around",
+        }}
+      >
+        {filteredAndSortedProducts.map((product) => (
           <CarProduct
             key={product.id}
             model={product.model}
             desc={product.description}
             img={product.image}
-            imgAlt={`vehiculo ${product.model}`}
+            imgAlt={`vehículo ${product.model}`}
             usage={product.usage}
             color={product.color}
             price={product.price}
