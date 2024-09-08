abc07d1de99f71ce76bdabef9867f52b14342b8c
maurojjzz <maurojim123@gmail.com>
2024-09-07 21:49:43 -0300

    button welcome page

--- src/components/welcomeSection/WelcomeSection.jsx
+++ src/components/welcomeSection/WelcomeSection.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import Header from "../header/Header.jsx";
+import ButtonMore from "../welcomeSection/buttonMore/ButtonMore";
 
 const WelcomeSection = () => {
   return (
@@ -15,9 +16,24 @@ const WelcomeSection = () => {
           xs: "url('/assets/images/welcome-mobile.jpg')",
           md: "url('/assets/images/welcome-desktop.jpg')",
         },
+        border: "1px solid #b3b3b2",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
       <Header />
+      <Box
+        sx={{
+          display: "flex",
+          flexGrow: 1,
+          alignItems: "flex-end", // Alinea el contenido al final del contenedor
+          justifyContent: "center", // Alinea el contenido al final horizontalmente
+          mb: "100px",
+          // mr: "10px",
+        }}
+      >
+        <ButtonMore />
+      </Box>
     </Box>
   );
 };
